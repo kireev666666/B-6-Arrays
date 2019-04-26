d7b2ca274e629fb17bc427da20a9dc422e742d70
kireev_6666666@outlook.com <kireev_6666666@outlook.com>
2019-04-26 15:39:50 +0300

    task6

--- B-6-Arrays-Strings/Program.cs
+++ B-6-Arrays-Strings/Program.cs
@@ -10,10 +10,37 @@ namespace Base.Lesson_6
     {
         static void Main(string[] args)
         {
-            Pyatnashki();
-            Console.ReadLine();
+            int[,] mass = new int[3, 3] { { 2, 4, 6 }, { 3, 5, 9 }, { 1, 7, 11 } };
+            int max;
+            for (int i = 0; i < 3; i++)
+            {
+                max = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (mass[i, j] > max) max = mass[i, j];
+                }
+                Console.WriteLine(max);
+            }
+            Console.ReadKey();
+
         }
 
+        public static void DMasiveSort()
+        {
+            Console.WriteLine("Заполните массив");
+            int a, b, c, d, e;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            e = Convert.ToInt32(Console.ReadLine());
+            int[] mass = new int[5] { a, b, c, d, e };
+            Array.Sort(mass);
+            for (int i = 0; i < mass.Length; i++)              
+                Console.Write(" " + mass[i]);
+            Console.ReadKey();
+        }
+       
         public static void Pyatnashki()
         {
             int[,] mass = new int[3, 3];
@@ -53,9 +80,34 @@ namespace Base.Lesson_6
 
         }
 
-        public static void PoemExample()
+        public static void  CutString()
         {
-            
+            Console.Write("Введите предложение: ");
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+                if (text.Length > 13)
+                {
+                    text = text.Remove(13) + "...";
+                }
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+
+        }
+                
+        public static void ReplaceInPoem()
+        {
+            Console.WriteLine("Введите стихотворение в одну строку разделяя строки точкой с запятой ");
+            string text = Console.ReadLine();
+            text = text.Replace('а', 'о').Replace("л", "ль").Replace("ть", "т");
+            string[] words = text.Split(';');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
